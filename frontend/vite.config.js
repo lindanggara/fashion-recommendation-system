1b11d1bc1c6f302f5de04620e97ea15001f3ee95
lindanggara <lindaanggaraw@gmail.com>
2026-06-13 22:36:16 +0700

    Add Dockerfile and Hugging Face config

---
+++ frontend/vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
+export default defineConfig({
+    plugins: [
+        react(),
+        visualizer({ filename: 'dist/stats.html', open: false, gzipSize: true, brotliSize: true })
+    ],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+                    'chart-vendor': ['recharts'],
+                    'icon-vendor': ['lucide-react'],
+                    'axios-vendor': ['axios'],
+                }
+            }
+        },
+        chunkSizeWarningLimit: 1000,
+        sourcemap: false,
+        minify: 'terser',
+        terserOptions: { compress: { drop_console: true, drop_debugger: true } }
+    },
+    server: { port: 5173 }
+});
